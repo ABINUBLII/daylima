af33059b858d5962f33db893e9aba72c4f25926c
ABINUBLII <nubliabi97@gmail.com>
2023-12-21 21:35:52 +0700

    variable dan manipulasi string

---
+++ challangeVar.js
@@ -0,0 +1,10 @@
+//challenge variable manipulasi string
+// tampilkan data menggunakan `
+let namaDepan = "samsul";
+let namaBelakang = "rijal";
+//output halo nama saya var 1 dan 2
+console.log(`halo nama saya ${namaDepan} ${namaBelakang} `);
+
+//ubah saya jadi aku
+let data = "hallo nama saya adit saya sedang belajar js";
+console.log(data.replaceAll("saya", "aku"));
